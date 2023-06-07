d99c9361111d33219fa7821ddafd14a8a562b668
Otmane Aatik <aaotmane@gmail.com>
2023-06-07 16:14:57 -0400

    mobile menu toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-console.log("connected!!!");
+const mobileMenuTrigger = document.getElementById('mobileMenuTrigger');
+const mobileMenu = document.querySelector('.nav-links-container');
+
+mobileMenuTrigger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    mobileMenuTrigger.classList.toggle('active');
+})
