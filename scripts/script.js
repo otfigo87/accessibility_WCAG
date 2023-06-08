b917e731a0d2580c641b877a9711456ce0a71cc6
Otmane Aatik <aaotmane@gmail.com>
2023-06-08 17:30:35 -0400

    javascript to improve accessibility

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
 const mobileMenuTrigger = document.getElementById('mobileMenuTrigger');
 const mobileMenu = document.querySelector('.nav-links-container');
+const navLinks = document.querySelectorAll('.nav-links li a');
 
 mobileMenuTrigger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-    mobileMenuTrigger.classList.toggle('active');
+  mobileMenu.classList.toggle("active");
+  mobileMenuTrigger.classList.toggle("active");
+  // Toggle Aria-Expanded Attribute True / False
+  if (mobileMenu.classList.contains("active")) {
+    mobileMenuTrigger.setAttribute("aria-expanded", "true");
+  } else {
+     mobileMenuTrigger.setAttribute("aria-expanded", "false");
+  }
 })
+
+// Provide Styling for Current Page Anchor Tag
+navLinks.forEach(link => {
+    if(link.href === window.location.href){
+        link.setAttribute('aria-current', 'page')
+    }
+})
+
